5aa2cb208164e029282201f19ed772d0cdd15e46
OGladfelter <ogladfelter@gmail.com>
2023-05-29 18:10:17 -0500

    got linechart working

--- js/script.js
+++ js/script.js
@@ -26,14 +26,18 @@ function drawHeatmap() {
     // icon allowing users to download screenshots
     L.control.bigImage().addTo(map);
 
-    // Get the data
-    d3.csv("data/meta_data.csv").then(function(data) {
-        data.forEach(function(d) {
-            d.lat = +d.lat;
-            d.lon = +d.lon;
+    d3.csv("data/sensor_data_with_loc.csv").then(function(sensors) {
+        // Get the data
+        d3.csv("data/meta_data.csv").then(function(data) {
+            data.forEach(function(d) {
+                d.lat = +d.lat;
+                d.lon = +d.lon;
 
-            // add marker to map
-            L.circle([d.lat, d.lon], {radius: 50}).addTo(map);
+                // add marker to map
+                L.circle([d.lat, d.lon], {radius: 50}).addTo(map)
+                //.bindTooltip(d.LCLid, {sticky: true})
+                .addEventListener('mouseover', tooltip.bind(null, d.LCLid, sensors.filter(s => s.LCLid == d.LCLid)), false);
+            });
         });
     });
 
@@ -46,29 +50,86 @@ function drawHeatmap() {
         map.removeLayer(mapTilesDark);
         map.removeLayer(mapTilesLight);
         map.removeLayer(mapTilesTerrain);
-        document.getElementById("backgroundColorPicker").style.display = "table-row";
     });
     document.getElementById("lightMapButton").addEventListener("click", function() { 
         // add leaflet map tiles
         map.removeLayer(mapTilesDark);
         map.removeLayer(mapTilesTerrain);
         map.addLayer(mapTilesLight);
-        document.getElementById("backgroundColorPicker").style.display = "none";
     });
     document.getElementById("darkMapButton").addEventListener("click", function() { 
         // add leaflet map tiles
         map.removeLayer(mapTilesLight);
         map.removeLayer(mapTilesTerrain);
         map.addLayer(mapTilesDark);
-        document.getElementById("backgroundColorPicker").style.display = "none";
     });
     document.getElementById("terrainMapButton").addEventListener("click", function() { 
         // add leaflet map tiles
         map.removeLayer(mapTilesLight);
         map.removeLayer(mapTilesDark);
         map.addLayer(mapTilesTerrain);
-        document.getElementById("backgroundColorPicker").style.display = "none";
     });
+}
+
+function tooltip(id, data, event) {
+    d3.select("#lineChart").select("svg").remove();
+
+    // set the dimensions and margins of the graph
+    let box = document.getElementById('lineChart');
+    let width = box.offsetWidth;
+
+    // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 20, bottom: 20, left: 20};
+    width = width - margin.left - margin.right;
+    var height = screen.height * .25 - margin.top - margin.bottom;
+
+    // parse the date / time
+    var parseTime = d3.timeParse("%Y-%m-%d");
+
+    // set the ranges
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+    // define the line
+    var valueline = d3.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.energy_median); });
+
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select("#lineChart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Get the data
+    data.forEach(function(d) {
+        // format the data
+        d.date = parseTime(d.day);
+        d.energy_median = +d.energy_median;
+    })
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function(d) { return d.energy_median; })]);
+
+    // Add the valueline path.
+    svg.append("path")
+        .data([data])
+        .attr("class", "line")
+        .attr("d", valueline);
+
+    // Add the x Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // Add the y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
 }
 
 // click divs to show / hide menus
@@ -81,13 +142,6 @@ document.getElementById("menuHeaderContainer3").addEventListener("click", functi
        document.getElementById("customizeMenuTable").style.display = "table";
        document.getElementById("menuButton3").innerHTML = "-";
     }
-});
-
-
-// customization menu item - background color
-document.getElementById("backgroundColor").addEventListener("input", function() { 
-    document.getElementById("map").style.background = this.value;
-    document.getElementsByTagName("body")[0].style.backgroundColor = this.value;
 });
 
 document.getElementById("printModal").addEventListener("click", function(){
